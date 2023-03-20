eb0df08ae8087cdeafd10d49b079f96c126ce9a0
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-03-20 14:01:07 +0100

    Patrolling the perimeter

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -32,7 +32,7 @@ public class State
         stage = EVENT.ENTER;
         player = _player;
     }
-    public virtual void Enter() { stage = EVENT.UPDATE;}
+    public virtual void Enter() { stage = EVENT.ENTER;}
     public virtual void Update() { stage = EVENT.UPDATE;}
     public virtual void Exit() { stage = EVENT.EXIT;}
 
@@ -46,5 +46,63 @@ public class State
             return nextState;
         }
         return this;
+    }
+}
+public class Idle : State{
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+    public override void Update()
+    {
+        if(Random.Range(0,100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle");
+        base.Exit();
+    }
+}
+public class Patrol: State
+{
+    int currentIndex = -1;
+        public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2;
+        agent.isStopped = false;
+    }
+    public override void Enter()
+    {
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+    public override void Update()
+    {
+        if(agent.remainingDistance < 1)
+        {
+            if(currentIndex >= GameEnvironment.Singleton.Checkpoints.Count -1)
+            {
+                currentIndex = 0;
+            } else currentIndex++;
+            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position);
+        }
+        base.Update();
+    }
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalking");
+        base.Exit();
     }
 }
